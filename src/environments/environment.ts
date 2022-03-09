c967078f99128846218979ca5b66144e34fa3254
Alireza <alirezaavarsaje@gmail.com>
2022-03-09 14:11:46 +0330

    cheat

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   // apiUrl: 'http://5f880da6-e7db-4d97-899a-fd530f7b5cf0.hsvc.ir:32165'
-  apiUrl: 'https://192.168.2.49:5001'
+  apiUrl: 'https://cloud-computing-api.darkube.app'
   // https://ccproj.darkube.app/
 };
